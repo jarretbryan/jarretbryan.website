9da22620cf2007aebf86e1a5d625143218e0552c
jarretbryan <jarret.bryan@gmail.com>
2018-10-15 11:32:21 -0400

    create json data for projects; try to render

--- src/components/project.js
+++ src/components/project.js
@@ -4,12 +4,10 @@ class Project extends Component {
     render() {
         return (
             <article className="center mw5 mw6-ns hidden ba mv4">
-                <h1 className="f4 bg-near-black white mv0 pv2 ph3 code">Title of card</h1>
+                <h1 className="f4 bg-near-black white mv0 pv2 ph3 code">hi</h1>
                 <div className="pa3 bt">
                     <p className="f6 f5-ns lh-copy measure mv0 sans-serif">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                        vero eos et accusam et justo duo dolores et ea rebum.
+                        {this.props.project.description}
                     </p>
                 </div>
             </article>
--- src/components/projectcontainer.js
+++ src/components/projectcontainer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Project from '../components/project'
 
+import data from '../data/projects.json'
+
 class ProjectContainer extends Component {
+
+    state = {
+        test: data
+    }
+
     render() {
         return (
             <div className="cf">
                 <div className="fl w-third pa2">
-                    <Project title="Radiowired"/>
+                    <Project project={data.projects[0]} />
                 </div>
                 <div className="fl w-third pa2">
                     <Project />
