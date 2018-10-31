d658490469e05069ee4a4ab7b1280fe159d6b41a
jarretbryan <jarret.bryan@gmail.com>
2018-10-30 23:38:06 -0400

    Edity styling in footer"

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
     return (
         <footer className="pv4 ph3 ph5-m ph6-l bg-near-black white code">
             <Social />
-            <small className="f6 db tc"> {(new Date().getFullYear())} &copy; Jarret Bryan <br/> Code + Design by Jarret Bryan</small>
+            <small className="f6 db tc">  {(new Date().getFullYear())} &copy; Jarret Bryan <br /> <span className="gold">// 🐝 </span> Code + Design by Jarret Bryan</small>
             <div className="tc mt3 code">
                 <small>This site was built using <a href="https://gatsbyjs.org" className="link dim gold">Gatsby</a> & <a href="http://tachyons.io/" className="link dim gold">Tachyons</a>. It is hosted p2p with <a href="https://datproject.org/" className="link dim gold">Dat</a> and <a href="https://hashbase.io/" className="link dim gold">#_hashbase</a>.</small>
             </div>
